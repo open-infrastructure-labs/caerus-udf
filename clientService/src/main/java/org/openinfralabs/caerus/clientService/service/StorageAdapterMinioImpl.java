f5721d190221ec393002989b0f49f90eb39c89c6
ywang529 <yong.wang@futurewei.com>
2021-01-21 15:58:53 -0500

    Adding part 1 of openfaas serverless direct invocation support: ndp service to invoke function based on payload info with storage request.

--- clientService/src/main/java/org/openinfralabs/caerus/clientService/service/StorageAdapterMinioImpl.java
+++ clientService/src/main/java/org/openinfralabs/caerus/clientService/service/StorageAdapterMinioImpl.java
@@ -1,8 +1,10 @@
 package org.openinfralabs.caerus.clientService.service;
 
+import com.amazonaws.services.s3.event.S3EventNotification;
 import io.minio.*;
 import io.minio.messages.Bucket;
 import org.apache.commons.io.IOUtils;
+import org.joda.time.DateTime;
 import org.openinfralabs.caerus.clientService.model.UdfInvocationMetadata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,16 +18,31 @@ import org.springframework.web.client.RestTemplate;
 import javax.annotation.PostConstruct;
 import java.io.*;
 import java.net.URI;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 import org.openinfralabs.caerus.clientService.model.Udf;
 import org.springframework.web.util.UriComponentsBuilder;
 
+
+// SERVERLESS related imports
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+
+
+import io.swagger.client.ApiException;
+import io.swagger.client.api.DefaultApi;
+import io.swagger.client.model.FunctionListEntry;
+
 @Service
 public class StorageAdapterMinioImpl implements StorageAdapter {
+
+    // adding conditional compiling flag
+    public static final boolean SERVERLESS = true;
+
     Logger logger = LoggerFactory.getLogger(StorageAdapterMinioImpl.class);
 
     final String udf_registry_service_uri = "http://localhost:8080/udf/";
@@ -34,7 +51,7 @@ public class StorageAdapterMinioImpl implements StorageAdapter {
     final String invocation_event_access = "get";
     final String invocation_event_delete = "delete";
     final String bucketNamePath = "bucketName";
-    final String objectKeyPath = "objectkey";
+    final String objectKeyPath = "objectKey";
     final String DEFAULT_INPUT_PARAMETERS_KEY = "inputParameters";
 
     @Autowired
@@ -51,7 +68,7 @@ public class StorageAdapterMinioImpl implements StorageAdapter {
             } else {
                 logger.info("Bucket already exists: " + bucket);
             }
-            
+
             // Upload input stream with headers and user metadata.
 
             // see if there is udf metadata
@@ -93,40 +110,95 @@ public class StorageAdapterMinioImpl implements StorageAdapter {
                 //1. validate via registry service
 
                 String udfKey = metadata.getName();
-                RestTemplate restTemplate = new RestTemplate();
+
+                if (!SERVERLESS) {
+                    RestTemplate restTemplate = new RestTemplate();
 
 
-                Udf udf = restTemplate.getForObject(udf_registry_service_uri+udfKey, Udf.class);
-                if (udf != null) {
-                    logger.info("found udf in the registry: " + udfKey);
-                } else {
-                    logger.warn("no udf found in the registry, so ignore udf invocation.");
-                    return;
+                    Udf udf = restTemplate.getForObject(udf_registry_service_uri + udfKey, Udf.class);
+                    if (udf != null) {
+                        logger.info("found udf in the registry: " + udfKey);
+                    } else {
+                        logger.error("no udf found in the registry, so ignore udf invocation.");
+                        return;
+                    }
+
+                    //2. TODO: need automate the deploy of udf
+
+                    //3. invoke udf via udf service
+
+                    RestTemplate udfRestTemplate = new RestTemplate();
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put(bucketNamePath, bucket);
+                    params.put(objectKeyPath, filename);
+
+                    String path = udf_docker_uri + bucket + "/" + filename;
+
+                    if (!userMetadata.isEmpty() && userMetadata.containsKey(DEFAULT_INPUT_PARAMETERS_KEY)) {
+                        String inputParametersCommaSeparated = userMetadata.get(DEFAULT_INPUT_PARAMETERS_KEY);
+                        path = path + "/" + "?" + DEFAULT_INPUT_PARAMETERS_KEY + "=" + inputParametersCommaSeparated;
+                    }
+                    ResponseEntity<String> responseEntity = udfRestTemplate.getForEntity(path, String.class, params);
+                    boolean isOK = responseEntity.getStatusCode().equals(HttpStatus.OK);
+                    if (isOK)
+                        logger.info("UDF invoked successfully: " + filename);
+                    else
+                        logger.error("UDF invoked failed: " + filename);
+
+                } else { //SERVERLESS MODE
+
+                    DefaultApi apiInstance = new DefaultApi();
+
+                    // check if the function name is in the openfaas deployed function list
+                    // don't re-throw exception here, just log the error, since we did main task of storage operations like put, delete...
+
+                    try {
+                        FunctionListEntry result = apiInstance.systemFunctionFunctionNameGet(udfKey);
+                        //System.out.println(result);
+                    } catch (ApiException e) {
+                        System.err.println("Exception when calling DefaultApi#systemFunctionFunctionNameGet");
+                        e.printStackTrace();
+                        logger.error("no function found in the Openfaas deployed function list, so ignore invocation: udfKey = " + udfKey);
+                        return;
+                    }
+
+
+                    JsonObject jsonObject = new JsonObject();
+                    jsonObject.addProperty(bucketNamePath, bucket);
+                    jsonObject.addProperty(objectKeyPath, filename);
+
+
+                    byte[] inputBytes = new byte[]{};
+
+                    if (!userMetadata.isEmpty() && userMetadata.containsKey(DEFAULT_INPUT_PARAMETERS_KEY)) {
+                        String inputParametersCommaSeparated = userMetadata.get(DEFAULT_INPUT_PARAMETERS_KEY);
+                        List<String> list = Arrays.asList(inputParametersCommaSeparated.split(","));
+                        String innerObjStr = new Gson().toJson(list);
+                        JsonElement jsonElement = new JsonParser().parse(innerObjStr);
+
+                        jsonObject.add(DEFAULT_INPUT_PARAMETERS_KEY, jsonElement);
+
+
+                        String jsonStr = jsonObject.toString();
+                        inputBytes = jsonStr.getBytes();
+                    }
+
+
+                    try {
+                        apiInstance.functionFunctionNamePost(udfKey, inputBytes);
+                    } catch (ApiException e) {
+                        System.err.println("Exception when calling DefaultApi#functionFunctionNamePost");
+                        logger.error("UDF invoked failed");
+                        e.printStackTrace();
+                        return;
+
+                    }
                 }
 
-                //2. TODO: need automate the deploy of udf
-
-                //3. invoke udf via udf service
-
-                RestTemplate udfRestTemplate = new RestTemplate();
-                Map<String, String> params = new HashMap<String, String>();
-                params.put(bucketNamePath, bucket);
-                params.put(objectKeyPath, filename);
-
-                String path = udf_docker_uri + bucket + "/" + filename;
-
-                if (!userMetadata.isEmpty() && userMetadata.containsKey(DEFAULT_INPUT_PARAMETERS_KEY)) {
-                    String inputParametersCommaSeparated = userMetadata.get(DEFAULT_INPUT_PARAMETERS_KEY);
-                    path = path + "/" + "?" + DEFAULT_INPUT_PARAMETERS_KEY + "=" + inputParametersCommaSeparated;
-                }
-                ResponseEntity<String> responseEntity = udfRestTemplate.getForEntity(path, String.class, params);
-                boolean isOK = responseEntity.getStatusCode().equals(HttpStatus.OK);
-                if (isOK)
-                    logger.info("UDF invoked successfully: " + filename);
-                else
-                    logger.error("UDF invoked failed: " + filename);
+                logger.info("UDF invoked successfully");
 
             }
+
 
         } catch (Exception e) {
             logger.error("Error occurred: " + e);
@@ -156,11 +228,11 @@ public class StorageAdapterMinioImpl implements StorageAdapter {
 
     @Override
     public void deleteFile(String bucket, String key, UdfInvocationMetadata metadata) {
-       try {
-           minioClient.removeObject(
-                   RemoveObjectArgs.builder().bucket(bucket).object(key).build());
-       } catch (Exception e) {
-           e.printStackTrace();
-       }
+        try {
+            minioClient.removeObject(
+                    RemoveObjectArgs.builder().bucket(bucket).object(key).build());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
