cfdf614a35a11e0e6841a4020a77a7a59a066998
ywang529 <yong.wang@futurewei.com>
2021-01-28 11:47:51 -0500

    NDP serverless direct invocation: a simple aws sdk client for object upload etc.

---
+++ awsjavaclient/src/main/java/S3Sample.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2010-2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.*;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
+
+import java.io.*;
+import java.util.UUID;
+
+/**
+ * This sample demonstrates how to make basic requests to Amazon S3 using the
+ * AWS SDK for Java.
+ * <p>
+ * <b>Prerequisites:</b> You must have a valid Amazon Web Services developer
+ * account, and be signed up to use Amazon S3. For more information on Amazon
+ * S3, see http://aws.amazon.com/s3.
+ * <p>
+ * Fill in your AWS access credentials in the provided credentials file
+ * template, and be sure to move the file to the default location
+ * (~/.aws/credentials) where the sample code will load the credentials from.
+ * <p>
+ * <b>WARNING:</b> To avoid accidental leakage of your credentials, DO NOT keep
+ * the credentials file in your source directory.
+ *
+ * http://aws.amazon.com/security-credentials
+ */
+public class S3Sample {
+
+    private static String caerusNdpServiceEndpoint = "http://localhost:8000";
+    private static String bucketName = "testbucket";
+    private static String keyName = "sample3.jpg";
+    private static String uploadFileName = "/home/ubuntu/images/new/sample3.jpg";
+    private static String FUNCTION_NAME_KEY = "function_name";
+    private static String FUNCTION_NAME_VALUE = "caerus-faas-spring-thumbnail";
+    private static String FUNCTION_INPUTPARAMETERS_KEY = "function_inputParameters";
+    private static String FUNCTION_INPUTPARAMETERS_VALUE = "400, 600";
+
+    public static void main(String[] args) throws IOException {
+        AWSCredentials credentials = new BasicAWSCredentials("YOUR-ACCESSKEYID", "YOUR-SECRETACCESSKEY");
+        ClientConfiguration clientConfiguration = new ClientConfiguration();
+        clientConfiguration.setSignerOverride("AWSS3V4SignerType");
+
+        AmazonS3 s3Client = AmazonS3ClientBuilder
+                .standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", Regions.US_EAST_1.name()))
+                .withPathStyleAccessEnabled(true)
+                .withClientConfiguration(clientConfiguration)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+
+        try {
+
+            // avoid 403 error:
+            System.setProperty("com.amazonaws.services.s3.disablePutObjectMD5Validation", "true");
+
+            System.out.println("Uploading a new object to S3 from a file\n");
+            File file = new File(uploadFileName);
+
+            // Upload a file as a new object with ContentType and title specified.
+            PutObjectRequest request = new PutObjectRequest(bucketName, keyName, file);
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.addUserMetadata(FUNCTION_NAME_KEY, FUNCTION_NAME_VALUE);
+            metadata.addUserMetadata(FUNCTION_INPUTPARAMETERS_KEY, FUNCTION_INPUTPARAMETERS_VALUE);
+            request.setMetadata(metadata);
+            s3Client.putObject(request);
+
+
+        } catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, which " + "means your request made it "
+                    + "to Amazon S3, but was rejected with an error response" + " for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, which " + "means the client encountered " + "an internal error while trying to "
+                    + "communicate with S3, " + "such as not being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+
+        }
+
+    }
+
+    /**
+     * Creates a temporary file with text data to demonstrate uploading a file
+     * to Amazon S3
+     *
+     * @return A newly created temporary file with text data.
+     *
+     * @throws IOException
+     */
+    private static File createSampleFile() throws IOException {
+        File file = File.createTempFile("aws-java-sdk-", ".txt");
+        file.deleteOnExit();
+
+        Writer writer = new OutputStreamWriter(new FileOutputStream(file));
+        writer.write("abcdefghijklmnopqrstuvwxyz\n");
+        writer.write("01234567890112345678901234\n");
+        writer.write("!@#$%^&*()-=[]{};':',.<>/?\n");
+        writer.write("01234567890112345678901234\n");
+        writer.write("abcdefghijklmnopqrstuvwxyz\n");
+        writer.close();
+
+        return file;
+    }
+
+    /**
+     * Displays the contents of the specified input stream as text.
+     *
+     * @param input
+     *            The input stream to display as text.
+     *
+     * @throws IOException
+     */
+    private static void displayTextInputStream(InputStream input) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        while (true) {
+            String line = reader.readLine();
+            if (line == null) break;
+
+            System.out.println("    " + line);
+        }
+        System.out.println();
+    }
+
+}
