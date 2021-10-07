725706f1a4db913ad82f35e2d3030cee53e554a1
ywang529 <yong.wang@futurewei.com>
2021-10-07 16:10:05 -0400

    Adding Caerus UDF Compiler performance result: src code changes.

--- examples/spark-udf/src/main/scala/org.openinfralabs.caerus.examples.spark-udf/submit-example-tax-discount-udf.scala
+++ examples/spark-udf/src/main/scala/org.openinfralabs.caerus.examples.spark-udf/submit-example-tax-discount-udf.scala
@@ -2,7 +2,7 @@ package org.openinfralabs.caerus.examples
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
-
+import org.apache.spark.util.SizeEstimator
 //1. Spark native: spark-submit --class org.openinfralabs.caerus.examples.SubmitExampleTaxDiscountUDF --master spark://10.124.48.60:7077 --driver-memory 5g target/spark-udf-1.0-SNAPSHOT.jar
 //2. Spark traslation: spark-submit --class org.openinfralabs.caerus.examples.SubmitExampleTaxDiscountUDF --master spark://10.124.48.60:7077 --driver-memory 5g --driver-class-path /root/caerus-spark-udf-compiler-from-rapids/udf-compiler/target/rapids-4-spark-udf_2.12-21.10.0-SNAPSHOT.jar --conf "spark.sql.extensions"="com.nvidia.spark.udf.Plugin" target/spark-udf-1.0-SNAPSHOT.jar
 object SubmitExampleTaxDiscountUDF {
@@ -17,7 +17,7 @@ object SubmitExampleTaxDiscountUDF {
       .getOrCreate()
 
     // TODO: can take parameters in
-    val prodDF = spark.read.parquet("hdfs://10.124.48.67:9000/testData10MRecords.parquet")
+    val prodDF = spark.read.parquet("hdfs://10.124.48.67:9000/testData1BRecords.parquet")
 
     prodDF.count
 
@@ -41,9 +41,21 @@ object SubmitExampleTaxDiscountUDF {
     val funcBasedResDF =  spark.sql("select prod, taxAndDiscountF(prod, amt) from  global_temp.products where taxAndDiscountF(prod, amt) > 50")
 
     funcBasedResDF.explain(true)
+    
+    println("file df size estimate:")
+    println(SizeEstimator.estimate(prodDF))
+
+
+    println("sql df size estimate:")
+    println(SizeEstimator.estimate(funcBasedResDF))
 
     // Measure time spent on the following task, for perf comparison
-    spark.time(funcBasedResDF.collect)
+    val countResult = 0
+    spark.time(print(funcBasedResDF.count))
+
+    //println("count result:")
+    //println(funcBasedResDF.count)
+
 
     spark.stop()
   }
